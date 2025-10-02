1cd95850caca528ef6a3592873fdfd707e56807b
Skimenok <iulybin44@gmail.com>
2025-10-02 18:51:17 +0300

    урок по циклам

--- main.js
+++ main.js
@@ -8,22 +8,21 @@ for (let i = 1; i <= 20; i++) {
 }
 
 // 2
-console.log("Задача 2");
-alert("Задача 2");
-let x = +prompt("Введите число", 1),
-  factorialX = 1;
-for (x; x > 0; x--) {
-  factorialX = factorialX * x;
-}
-console.log(factorialX);
+console.log("Задача 2");                                                                                              
+alert("Задача 2");                                                                                              
+let x = +prompt("Введите число", 1),                                                                                              
+  factorialX = 1;                                                                                              
+for (x; x > 0; x--) {                                                                                              
+  factorialX = factorialX * x;                                                                                              
+}                                                                                              
+console.log(factorialX);                                                                                              
 
 // 3
 console.log("Задача 3");
-let board = "";
-row = 1;
-el = true;
+let board = "",
+  el = true;
 
-while (row < 9) {
+for (let i = 1; i <= 8; i++) {
   for (let i = 0; i < 8; i++) {
     if (el == true) {
       board += "* ";
@@ -34,7 +33,6 @@ while (row < 9) {
     }
   }
   board += "\n";
-  row++;
-  el = el == true ? false : true
+  el = el == true ? false : true;
 }
 console.log(board);
